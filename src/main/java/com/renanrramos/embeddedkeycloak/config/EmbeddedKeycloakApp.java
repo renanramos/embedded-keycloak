36410ff68831916cd57bb83522a5274ddbbb2ea6
Renan Ramos <renan.ramos@inatel.br>
2020-10-24 01:21:31 -0300

    Remocao de import de realm customizado

--- src/main/java/com/renanrramos/embeddedkeycloak/config/EmbeddedKeycloakApp.java
+++ src/main/java/com/renanrramos/embeddedkeycloak/config/EmbeddedKeycloakApp.java
@@ -7,16 +7,11 @@ import java.util.NoSuchElementException;
 
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.services.managers.ApplianceBootstrap;
-import org.keycloak.services.managers.RealmManager;
 import org.keycloak.services.resources.KeycloakApplication;
 import org.keycloak.services.util.JsonConfigProviderFactory;
-import org.keycloak.util.JsonSerialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 import com.renanrramos.embeddedkeycloak.model.AdminUser;
 import com.renanrramos.embeddedkeycloak.properties.KeycloakServerProperties;
@@ -39,7 +34,6 @@ public class EmbeddedKeycloakApp extends KeycloakApplication {
 	public EmbeddedKeycloakApp() {
 		super();
 		createMasterRealmAdminUser();
-		createEasyShoppingRealm();
 	}
 
 	private void createMasterRealmAdminUser() {
@@ -53,21 +47,6 @@ public class EmbeddedKeycloakApp extends KeycloakApplication {
 			session.getTransactionManager().commit();			
 		} catch (Exception e) {
 			LOG.warn("Couldn't create keycloak master admin user: {}", e.getMessage());
-			session.getTransactionManager().rollback();
-		}
-		session.close();
-	}
-
-	private void createEasyShoppingRealm() {
-		KeycloakSession session = getSessionFactory().create();
-		try {
-			session.getTransactionManager().begin();
-			RealmManager manager = new RealmManager(session);
-			Resource realmImportFile = new ClassPathResource(keycloakServerProperties.getEasyShoppingRealmFile());
-			manager.importRealm(JsonSerialization.readValue(realmImportFile.getInputStream(), RealmRepresentation.class));
-			session.getTransactionManager().commit();
-		}catch (Exception e) {
-			LOG.warn("Failed to import Realm json file: {}", e.getMessage());
 			session.getTransactionManager().rollback();
 		}
 		session.close();
